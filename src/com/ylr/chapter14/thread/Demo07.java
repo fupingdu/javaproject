78170df1ec18b9dd5c65339c064fe495113d5fe6
fupingdu <764514770@qq.com>
2021-11-12 00:08:49 +0800

    集合之操作

---
+++ src/com/ylr/chapter14/thread/Demo07.java
@@ -0,0 +1,9 @@
+package com.ylr.chapter14.thread;
+
+public class Demo07 {
+    public static void main(String[] args) {
+//        String s = String.valueOf(1);
+        //面试题：这行代码被注释了，为什么还有输出：Hello World!
+        //  \u000d System.out.println("Hello World!");
+    }
+}
